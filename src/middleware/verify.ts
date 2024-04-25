12a7e2a75c2f74d291a6d2900d20d248332de646
chann44 <44chansw@gmail.com>
2024-04-25 12:01:44 +0530

    Feat: made changes in auth strategy

--- src/middleware/verify.ts
+++ src/middleware/verify.ts
@@ -2,7 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
 export const verifyJwt = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.cookies.token;
+  console.log("request is coming");
+  const token = req.headers?.authorization?.split(" ")[1];
   const privateKey = process.env.JWT_TOKEN_SECRETE;
   if (token && privateKey) {
     try {
